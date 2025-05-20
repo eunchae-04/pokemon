77c92e69e4d19183e00a12662f8b703a83290e49
eunchae-04 <rhdmsco1163@naver.com>
2025-05-20 15:37:24 +0900

    Add files via upload

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
 export default defineConfig({
+  base: '/pokemon/', // ← '저장소 이름'으로 설정
   plugins: [react()],
 })
